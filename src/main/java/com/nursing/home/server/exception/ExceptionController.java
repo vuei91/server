edae89a052d85f45a8873fd9dae40bd49a220898
vuei91 <wkwk2805u@naver.com>
2024-04-11 15:17:25 +0900

    Exception 파일 이름 변경

--- src/main/java/com/nursing/home/server/exception/ExceptionController.java
+++ src/main/java/com/nursing/home/server/exception/ExceptionController.java
@@ -1,15 +1,16 @@
 package com.nursing.home.server.exception;
 
+import com.nursing.home.server.common.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class AllExceptionHandler {
+public class ExceptionController {
     @ExceptionHandler({Exception.class})
-    public ResponseEntity<String> ResponseException () {
-        System.out.println("에러란 에러는 여기에 다 걸리려나?");
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error");
+    public ApiResponse<String> ResponseException (Exception e) {
+        e.printStackTrace();
+        return ApiResponse.error(e.getMessage());
     }
 }
