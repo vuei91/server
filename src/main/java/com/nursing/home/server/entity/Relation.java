59eaf400aefe4415806ee5171537c09483b9a53c
vuei91 <wkwk2805u@naver.com>
2024-04-12 15:12:18 +0900

    Relation 엔티티 추가 보호자와 환자의 관계를 나타냄

---
+++ src/main/java/com/nursing/home/server/entity/Relation.java
@@ -0,0 +1,25 @@
+package com.nursing.home.server.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Relation {
+    @Id
+    @GeneratedValue
+    @Column(name = "relation_id")
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+    @CreationTimestamp // INSERT 시 자동으로 값을 채워줌
+    private final LocalDateTime createdAt = LocalDateTime.now();
+    @UpdateTimestamp // UPDATE 시 자동으로 값을 채워줌
+    private final LocalDateTime updatedAt = LocalDateTime.now();
+}
