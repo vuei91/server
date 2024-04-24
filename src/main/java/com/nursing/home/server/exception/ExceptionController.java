fd8bf2c39a4ff8ca93f2e1d1b9bfa4a3d83f40b1
vuei91 <wkwk2805u@naver.com>
2024-04-24 18:33:53 +0900

    익셉션 우선순위 처리 및 인증 문제 관련 익셉션처리

--- src/main/java/com/nursing/home/server/exception/ExceptionController.java
+++ src/main/java/com/nursing/home/server/exception/ExceptionController.java
@@ -1,9 +1,10 @@
 package com.nursing.home.server.exception;
 
 import com.nursing.home.server.common.ApiResponse;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+@Order(2)
 @RestControllerAdvice
 public class ExceptionController {
 
--- src/main/java/com/nursing/home/server/jwt/JwtExceptionHandler.java
+++ src/main/java/com/nursing/home/server/jwt/JwtExceptionHandler.java
@@ -1,27 +1,36 @@
 package com.nursing.home.server.jwt;
 
+import com.nursing.home.server.common.ApiResponse;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Order(1)
 @RestControllerAdvice
 public class JwtExceptionHandler {
 
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<ApiResponse<String>> signatureExceptionHandler() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("잘못된 토큰입니다"));
+    }
+
     @ExceptionHandler(ExpiredJwtException.class)
-    public ResponseEntity<String> expiredJwtExceptionHandler() {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("만료된 토큰이다");
+    public ResponseEntity<ApiResponse<String>> expiredJwtExceptionHandler() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("만료된 토큰이다"));
     }
 
     @ExceptionHandler(MalformedJwtException.class)
-    public ResponseEntity<String> malformedJwtExceptionHandler() {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않은 토큰입니다");
+    public ResponseEntity<ApiResponse<String>> malformedJwtExceptionHandler() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("유효하지 않은 토큰입니다"));
     }
 
     @ExceptionHandler(SecurityException.class)
-    public ResponseEntity<String> securityExceptionHandler() {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("보안 예외 처리");
+    public ResponseEntity<ApiResponse<String>> securityExceptionHandler() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("보안 예외 처리"));
     }
 }
