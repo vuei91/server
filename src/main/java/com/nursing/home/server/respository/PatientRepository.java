bbd9726f7816c9fe300bb0ddbe5dd968a66d002e
vuei91 <wkwk2805u@naver.com>
2024-04-26 20:36:36 +0900

    relation에서 patient 얻어내기

--- src/main/java/com/nursing/home/server/respository/PatientRepository.java
+++ src/main/java/com/nursing/home/server/respository/PatientRepository.java
@@ -1,7 +1,11 @@
 package com.nursing.home.server.respository;
 
 import com.nursing.home.server.entity.Patient;
+import com.nursing.home.server.entity.Relation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    List<Patient> findAllByRelationsIn(List<Relation> relations);
 }
