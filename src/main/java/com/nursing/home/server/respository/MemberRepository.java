fc01855ee237a15ff2bba9de0610406440af65e8
vuei91 <wkwk2805u@naver.com>
2024-04-26 20:34:12 +0900

    relation 값 추가

--- src/main/java/com/nursing/home/server/respository/MemberRepository.java
+++ src/main/java/com/nursing/home/server/respository/MemberRepository.java
@@ -1,10 +1,12 @@
 package com.nursing.home.server.respository;
 
 import com.nursing.home.server.entity.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @EntityGraph(attributePaths = {"relations"})
     Optional<Member> findByUsername(String username);
 }
